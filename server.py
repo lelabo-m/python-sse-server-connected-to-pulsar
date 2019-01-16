0e95dd6bfd2d0368adb6217c069a1b2a6d57493b
lelabo_m <marc.lelabourier@gmail.com>
2019-01-16 16:11:46 +0100

    example of server

---
+++ server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import flask
+import pulsar
+
+
+app = flask.Flask(__name__)
+client = pulsar.Client('pulsar://localhost:6650')
+consumer = client.subscribe('my-topic', 'my-subscription')
+
+
+def event_stream():
+    while True:
+        msg = consumer.receive()
+        consumer.acknowledge(msg)
+        yield ("Received message '{}' id='{}'".format(msg.data(), msg.message_id()))
+
+
+
+@app.route('/stream')
+def stream():
+    return flask.Response(event_stream(), mimetype="text/event-stream")
+
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
